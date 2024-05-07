a15cff789219092e8ab0cf09319e4f93cd3708f9
tanyajohari <johtanya11@gmail.com>
2024-05-07 21:47:27 +0530

    [Tanya] Updated OOPs approach

--- oopsapproach/src/main/java/OOPApproach.java
+++ oopsapproach/src/main/java/OOPApproach.java
@@ -1,9 +1,11 @@
 import processing.core.PApplet;
 
 class Ball {
-    public static final int DIAMETER = 20;
-    private float positionX, positionY;
-    private float speed;
+
+    public static final int BALL_SIZE = 20;
+    private float positionX;
+    private final float positionY;
+    private final float speed;
 
     public Ball(float positionX, float positionY, float speed) {
         this.positionX = positionX;
@@ -16,32 +18,34 @@ class Ball {
     }
 
     public void display(PApplet sketch) {
-        sketch.ellipse(positionX, positionY, DIAMETER, DIAMETER);
+        sketch.ellipse(positionX, positionY, BALL_SIZE, BALL_SIZE);
     }
 }
 public class OOPApproach extends PApplet {
 
-    public static final int HEIGHT = 500;
-    public static final int WIDTH = 640;
+    public static final int NUM_BALLS = 4;
+    public static final int SCREEN_HEIGHT = 500;
+    public static final int SCREEN_WIDTH = 640;
+    public static final float[] HEIGHT_FRACTIONS = {1.0f/5, 2.0f/5, 3.0f/5, 4.0f/5};
+    public static final float[] BALL_SPEED = {1, 2, 3, 4};
 
     Ball[] balls = new Ball[4];
 
     @Override
     public void settings() {
-        size(WIDTH, HEIGHT);
+        size(SCREEN_WIDTH, SCREEN_HEIGHT);
     }
 
     @Override
     public void setup() {
-        balls[0] = new Ball(0, HEIGHT / 5, 1);
-        balls[1] = new Ball(0, 2 * HEIGHT / 5, 2);
-        balls[2] = new Ball(0, 3 * HEIGHT / 5, 3);
-        balls[3] = new Ball(0, 4 * HEIGHT / 5, 4);
+        paintWhite();
+        for (int i = 0; i < NUM_BALLS; i++) {
+            balls[i] = new Ball(0, HEIGHT_FRACTIONS[i] * SCREEN_HEIGHT, BALL_SPEED[i]);
+        }
     }
 
     @Override
     public void draw() {
-//        paintWhite();
         for (Ball ball : balls) {
             ball.update();
             ball.display(this);
